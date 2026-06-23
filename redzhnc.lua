b796b7052147a39275573f619e9b5887ad096c9c
napun87 <accmaruthunhat@gmail.com>
2026-06-23 16:20:42 +0700

    Create redzhnc.lua

---
+++ redzhnc.lua
@@ -0,0 +1,5 @@
+local Settings = {
+    JoinTeam = "Pirates"; -- Pirates / Marines
+    Translator = true;   -- true / false
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dev-Hinishi/Hinishi-Hub/refs/heads/main/Freemium.lua"))(Settings)
